3260967282691d80b5687bf2feef68a2533430aa
itsjo <itsjo@JOELSBOOK4>
2026-05-01 19:18:35 -0400

    misc

--- Program.cs
+++ Program.cs
@@ -42,11 +42,11 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseMiddleware<FinanceTracker.Middleware.ExceptionHandler>();
 app.UseCors("AllowFrontend");
