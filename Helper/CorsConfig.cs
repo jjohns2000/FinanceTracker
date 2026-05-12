c660faf0c4cc9299e012157b5ef11720abfd5f78
itsjo <itsjo@JOELSBOOK4>
2026-05-12 15:34:12 -0400

    misc

--- Helper/CorsConfig.cs
+++ Helper/CorsConfig.cs
@@ -13,7 +13,8 @@
                             "http://localhost:5173",
                             "http://localhost:3000",
                             "http://10.0.0.27:5173", //change here if the device IP changes
-                            "https://salmon-river-048dde10f.7.azurestaticapps.net" //Azure static apps
+                            "https://salmon-river-048dde10f.7.azurestaticapps.net", //Azure static apps
+                            "https://financetracker.itsjoeljohnson.com/"
                         )
                         .AllowAnyHeader()
                         .AllowAnyMethod();
